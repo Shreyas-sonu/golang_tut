f756029db460bd68b75bd32dd45720fa21beae65
Shreyas <sonureigns007@gmail.com>
2024-01-22 19:51:56 +0530

    array trial

--- tut_new/main.go
+++ tut_new/main.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	card := newCard()
-	fmt.Println(card)
+	cards := []string{"Card1", "Card2", "Card3"}
+	cards = append(cards, "Card4")
+	cards = append(cards, newCard())
+	for ind, card := range cards {
+		fmt.Println(ind, card)
+	}
 }
 func newCard() string {
 	return "A Plain Card"
 }
-
-// func newCard() int {
-// 	return 1
-// }
