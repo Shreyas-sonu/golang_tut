98479e4e77ea004a6b3a95c77397f783a7238bc8
Shreyas <sonureigns007@gmail.com>
2024-01-22 16:22:07 +0530

    variable declaration in go

--- tut_new/main.go
+++ tut_new/main.go
@@ -1,9 +1,6 @@
 package main
 
-import "fmt"
-
 func main() {
-	// var card string = "Ace of Spades"
-	card := "Ace of Spades" // short syntax for varibale declaration
-	fmt.Println(card)
+	// var card string="A plain card"
+	card:="A plain card"
 }
