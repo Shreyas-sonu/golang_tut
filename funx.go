db1091f5db62b009eaba17c01ada4ace7aca2946
Shreyas <sonureigns007@gmail.com>
2024-02-25 23:47:34 +0530

    func practice in go

---
+++ funx.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+import "rsc.io/quote"
+
+func main1() {
+    fmt.Println(quote.Go())
+}
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+	func main(){
+		fmt.Print("Hello world");
+		fmt.Println("I");//prints and moves execution to next line
+		fmt.Println("am");
+		fmt.Println("Groot");
+		fmt.Print("I am ");
+		fmt.Println("Ironman");
+		var villain="Thanos";
+		fmt.Println("----------------------------");
+		fmt.Println("i am ---- ",villain);
+		fmt.Println("----------------------------");
+		const hero="Tony Stark";
+		fmt.Printf("%v escape chars\n",villain)later after comma
+		fmt.Printf("%v said i am Iron man\n",hero)
+			fmt.Println(quote.Go())
+	}
